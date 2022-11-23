221a0a2fd579ae16e87fc9ddaa010909777b0114
MandS <monika.domaradzkaa@gmail.com>
2022-11-23 19:31:08 +0000

    case

--- src/Kotki.java
+++ src/Kotki.java
@@ -1,9 +1,34 @@
 public class Kotki{
     public static void main(String[] args) {
         String[] catname=new String[]{"Burek", "Mruczek", "Iskierka", "Pussy"};
-        for (int i=0;i<catname.length; i++){
+       for (int i=0;i<catname.length; i++){
             System.out.println(catname[i]);
         }
+
+        int x=3;
+        int y;
+        System.out.println(x++);//4
+        System.out.println(++x);//5
+        System.out.println(x);//5
+        y = x++;//5
+        System.out.println (y);//5
+        y = ++x;//7
+        System.out.println (y);//7
+        System.out.println (++y);//8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/Switch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int inputNumber= scanner.nextInt();
+        switch (inputNumber){
+            case 1:
+                System.out.println("case1");
+                break;
+            case 2:
+                System.out.println("case2");
+                break;
+                case 3:
+                    System.out.println("case3");
+                    break;
+
+        }
+
+    }
+}
