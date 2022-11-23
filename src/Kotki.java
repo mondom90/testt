d88f9d67b387cbb5f7ea8669c1ee69beb5acf8f8
MandS <monika.domaradzkaa@gmail.com>
2022-11-23 18:19:08 +0000

    Kotki

---
+++ src/Kotki.java
@@ -0,0 +1,12 @@
+public class Kotki{
+    public static void main(String[] args) {
+        String[] catname=new String[]{"Burek", "Mruczek", "Iskierka", "Pussy"};
+        for (int i=0;i<catname.length; i++){
+            System.out.println(catname[i]);
+        }
+
+
+
+
+    }
+}
